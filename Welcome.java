bbfab2fc49aa23a5fa7b9ed88c4b3feed99e9101
Sagar Kanthale <sagark@gmail.com>
2024-02-05 12:31:49 +0530

    new file added

---
+++ Welcome.java
@@ -0,0 +1,14 @@
+package com.epps.beans;
+
+import java.io.UnsupportedEncodingException;
+
+public class Welcome {
+
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		EncodeDecodeUtils encodeDecodeUtils = new EncodeDecodeUtils();
+		String encodeString  = encodeDecodeUtils.encode("123124Sagar123");
+		System.out.println(encodeString);
+		String decodeString = encodeDecodeUtils.decode(encodeString);
+		System.out.println(decodeString);
+	}
+}
